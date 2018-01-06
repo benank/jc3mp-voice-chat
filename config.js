c6e5320f4ea60b92b8b6b514dd23fb3e3fa5fe26
benank <sharpenedimages@gmail.com>
2018-01-05 17:28:02 -0800

    Add spatial audio rendering
    Using resonance audio sdk

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = 
 {
     talk_key: 'Q', // Default push to talk key
-    min_distance: 3, // Distance until volume starts decreasing
-    max_distance: 35, // Maximum distance until volume is 0
+    min_distance: 10, // Distance until volume starts decreasing
+    max_distance: 50, // Maximum distance until volume is 0
     host: 'MY SERVER IP', // Your server's IP here
     port: 9000 // Port to use
 }
